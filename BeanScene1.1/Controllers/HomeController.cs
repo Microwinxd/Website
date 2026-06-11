c70f7620f78f63b55adab024bcd43dc267cf107b
Microwinxd <samusenkodenis14@gmail.com>
2026-06-11 22:33:06 +1000

    edited menu page

--- BeanScene1.1/Controllers/HomeController.cs
+++ BeanScene1.1/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace BeanScene101._1.Controllers
         {
             return View();
         }
+        public IActionResult Menu()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
